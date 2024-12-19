eee412744c7dd3e03a50d51f3bc2a83f7d85c730
Lizandra Malta <lizlrmalta@gmail.com>
2024-12-19 00:37:44 -0300

    desenvolvendo tela de histórico

---
+++ src/components/HistoryCard.tsx
@@ -0,0 +1,44 @@
+import { Heading, HStack, Text, VStack } from '../../gluestack-components'
+
+type Props = {
+  group: string
+  name: string
+  time: string
+}
+
+export function HistoryCard({ group, name, time }: Props) {
+  return (
+    <HStack
+      w="$full"
+      px="$5"
+      py="$4"
+      alignItems="center"
+      gap="$0.5"
+      bgColor="$gray600"
+    >
+      <VStack flex={1}>
+        <Heading
+          fontSize="$md"
+          lineHeight="$md"
+          color="$white"
+          numberOfLines={1}
+          textTransform="capitalize"
+        >
+          {group}
+        </Heading>
+        <Text
+          fontSize="$lg"
+          lineHeight="$lg"
+          color="$gray100"
+          numberOfLines={2}
+          textTransform="capitalize"
+        >
+          {name}
+        </Text>
+      </VStack>
+      <Text fontSize="$lg" lineHeight="$lg" color="$gray300">
+        {time}
+      </Text>
+    </HStack>
+  )
+}
--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,10 +1,100 @@
 import { ScreenHeader } from '@components/ScreenHeader'
-import { VStack } from '../../gluestack-components'
+import { Box, Heading, Text, VStack } from '../../gluestack-components'
+import { HistoryCard } from '@components/HistoryCard'
+import { useCallback, useState } from 'react'
+import { SectionList } from 'react-native'
+import { useFocusEffect } from '@react-navigation/native'
+
+type Data = {
+  title: string
+  data: {
+    group: string
+    name: string
+    time: string
+  }[]
+}[]
+
+const mockHistory = [
+  {
+    title: '22.07.24',
+    data: [
+      {
+        group: 'Costas',
+        name: 'Puxada frontal',
+        time: '08:50'
+      },
+      {
+        group: 'Costas',
+        name: 'Remada unilateral',
+        time: '08:32'
+      }
+    ]
+  },
+  {
+    title: '21.07.24',
+    data: [
+      {
+        group: 'Costas',
+        name: 'Puxada frontal',
+        time: '11:24'
+      }
+    ]
+  }
+]
 
 export function History() {
+  const [history, setHistory] = useState<Data>([])
+
+  useFocusEffect(
+    useCallback(() => {
+      setHistory(mockHistory)
+    }, [])
+  )
+
   return (
     <VStack flex={1}>
       <ScreenHeader title="Histórico de Exercícios" />
+      <SectionList
+        sections={history}
+        keyExtractor={(item) => JSON.stringify(item)}
+        renderItem={({ item }) => (
+          <HistoryCard group={item.group} name={item.name} time={item.time} />
+        )}
+        renderSectionHeader={({ section }) => (
+          <Heading
+            color="$gray200"
+            fontSize="$md"
+            lineHeight="$md"
+            mt="$10"
+            mb="$3"
+          >
+            {section.title}
+          </Heading>
+        )}
+        style={{
+          paddingHorizontal: 32
+        }}
+        contentContainerStyle={[
+          history.length === 0 && { flex: 1, justifyContent: 'center' },
+          {
+            paddingBottom: 12
+          }
+        ]}
+        ItemSeparatorComponent={() => <Box h="$3" />}
+        ListEmptyComponent={() => (
+          <Text
+            color="$gray100"
+            textAlign="center"
+            fontSize="$md"
+            lineHeight="$md"
+          >
+            Não há exercícios registrados ainda. {'\n'}
+            Vamos fazer exercícios hoje?
+          </Text>
+        )}
+        stickyHeaderHiddenOnScroll
+        stickySectionHeadersEnabled={false}
+      />
     </VStack>
   )
 }
